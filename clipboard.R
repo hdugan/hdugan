560de9ba1ef19843b248b40625fe8c5b64f26618
hdugan <hilarydugan@gmail.com>
2015-05-27 09:39:14 -0500

    added comma separated value capability

--- clipboard.R
+++ clipboard.R
@@ -1,6 +1,10 @@
 #function to export to clipboard
-clipboard <- function(df) {
-  clip <- pipe("pbcopy", "w")                       
-  write.table(df, file=clip,row.names=F,col.names=F)                               
+clipboard <- function(df,csv=FALSE) {
+  clip <- pipe("pbcopy", "w")
+  if (csv == F) {
+    write.table(df,file=clip,row.names=F,col.names=F,quote=F)      
+  } else if (csv == T){
+    write.table(df,file=clip,row.names=F,col.names=F,quote=F,sep=',')  
+  }                          
   close(clip)
 }
