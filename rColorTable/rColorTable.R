ab38c652583315c6ffcff95f47262879b0afa045
hdugan <hilarydugan@gmail.com>
2016-06-01 16:50:59 -0500

    improved code

--- rColorTable/rColorTable.R
+++ rColorTable/rColorTable.R
@@ -1,3 +1,5 @@
+
+#### Make 2-page color table of all colors available in R ####
 # R colors minus 100 shades of grey 
 cols = colors()[c(2:151,362:657)]
 
@@ -9,11 +11,11 @@ for (i in 1:4){
 dupNames = gsub(pattern = '1',replacement = '',x = dupCols[[1]])
 
 # All Color Names 
-allNames = a[!grepl('1',a) & !grepl('2',a) & !grepl('3',a) & !grepl('4',a)]
+allNames = cols[!grepl('1',cols) & !grepl('2',cols) & !grepl('3',cols) & !grepl('4',cols)]
 indx = match(dupNames,allNames)
 
 # Output color table 
-output = data.frame(col = c,c1=NA,c2=NA,c3=NA,c4=NA,stringsAsFactors = F)
+output = data.frame(col = cols,c1=NA,c2=NA,c3=NA,c4=NA,stringsAsFactors = F)
 for (i in 1:4){
   output[,i+1][indx] = dupCols[[i]]
 }
@@ -22,28 +24,12 @@ for (i in 1:4){
 x1 = c(0,3,3.5,4,4.5)
 x2 = c(3,3.5,4,4.5,5)
 
-# Color chart
-pdf('~/Downloads/rColor.pdf')
-par(mar=c(0,6,0,6))
-plot(0, type="n", ylab="", xlab="",axes=FALSE, ylim=c(138,0), xlim=c(1,5))
-axis(2,at = c(1:138),labels = c,las=1,cex.axis=0.35)
-axis(4,at= indx,labels = b1names,cex.axis=0.35,las=1)
-for (j in 1:(138-1)) {
-  for (i in 1:5) {
-    #k = j*5 + i
-    if (!is.na(output[j,i])){
-      rect(x1[i],j-0.5, x2[i],j+0.5, border="black", col=output[j,i])
-    }
-  }
-}
-dev.off()
-
 # 2 page color chart 
-pdf('~/Dropbox/Scripts/hdugan/rColorTable/rColorTable.pdf',height = 10)
+pdf('rColorTable.pdf',height = 10)
 par(mar=c(0,6,0,6))
 # First page
 plot(0, type="n", ylab="", xlab="",axes=FALSE, ylim=c(69,0), xlim=c(1,5))
-axis(2,at = allNames(1:69),labels = allNames[1:69],las=1,cex.axis=0.6)
+axis(2,at = 1:69,labels = allNames[1:69],las=1,cex.axis=0.6)
 axis(4,at= indx[indx<70],labels = allNames[indx[indx<70]],cex.axis=0.6,las=1)
 for (j in 1:69) {
   for (i in 1:5) {
@@ -55,7 +41,7 @@ for (j in 1:69) {
 }
 # Second Page 
 plot(0, type="n", ylab="", xlab="",axes=FALSE, ylim=c(138,70),xlim=c(1,5))
-axis(2,at = allNames(70:138),labels = allNames[70:138],las=1,cex.axis=0.6)
+axis(2,at = 70:138,labels = allNames[70:138],las=1,cex.axis=0.6)
 axis(4,at= indx[indx>=70],labels = allNames[indx[indx>=70]],cex.axis=0.6,las=1)
 for (j in 70:138) {
   for (i in 1:5) {
